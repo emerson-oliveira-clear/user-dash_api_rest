7e2a6b84f4aa6d4dc11972c4e6a28d958087fe40
emerson-oliveira-clear <emerson.oliveira.clear>
2023-08-11 16:55:25 -0300

    finishing project

--- public/models/user.js
+++ public/models/user.js
@@ -80,7 +80,7 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
     }
 
     toJSON () {
@@ -105,11 +105,11 @@ class User {
 
             if (this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
 
             } else {
 
-                promise = HttpRequest.post(`/users`, this.toJSON());
+                promise = Fetch.post(`/users`, this.toJSON());
 
             }
 
@@ -131,7 +131,7 @@ class User {
 
     remove(){
 
-      return  HttpRequest.delete(`/users/${this.id}`)
+      return  Fetch.delete(`/users/${this.id}`)
 
     }
 
