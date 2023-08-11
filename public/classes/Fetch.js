ce454305cbcfa97f455a6d74f07b30469784f143
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-11 12:56:18 -0300

    finishing project

---
+++ public/classes/Fetch.js
@@ -0,0 +1,58 @@
+class Fetch {
+
+    static get(url, params = {}) {
+
+        return Fetch.request('GET', url, params);
+        
+    }
+
+    static delete(url, params = {}) {
+
+        return Fetch.request('DELETE', url, params);
+        
+    }
+
+    static put(url, params = {}) {
+
+        return Fetch.request('PUT', url, params);
+        
+    }
+
+    static post(url, params = {}) {
+
+        return Fetch.request('POST', url, params);
+        
+    }
+
+    static request(method, url, params = {}) {
+
+        return new Promise((resolve, reject) => {
+
+            let request = new Request(url,{
+                method:
+            })
+
+
+            fetch(url).then(response =>{
+
+                response.json().then(json =>{
+
+                    resolve(json);
+
+                }).catch(e =>{
+
+                    reject(e);
+
+                });
+
+            }).catch(e =>{
+
+                reject(e);
+
+            });
+
+        });
+
+    }
+
+}
